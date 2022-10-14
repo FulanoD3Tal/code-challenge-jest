5bce50d8b45e0483fa1e202257750bd12eaff0fe
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2022-10-14 09:58:28 -0500

    Add babel for escma use

--- challenge.test.js
+++ challenge.test.js
@@ -1,5 +1,7 @@
-describe('Suite', () => { 
-    it('should ', () => {
-        
-    });
- })
+import challenge from './challenge';
+
+describe('Suite', () => {
+  it('should ', () => {
+    expect(challenge()).toBe(undefined);
+  });
+});
